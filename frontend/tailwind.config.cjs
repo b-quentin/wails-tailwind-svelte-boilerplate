22dced7a474ae08373ce6620ddc6a581a959b529
bquentin <bquentin@protonmail.com>
2024-07-13 10:15:59 +0200

    init

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,svelte}", // Assurez-vous d'inclure tous les fichiers pertinents
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
